09019714bd2dac4d2af865a2416c3b016d92d08d
alist315 <aaron.reinheimner315@gmail.com>
2019-04-13 16:33:32 -0400

    things are coming together

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Project-1-Marvel-Index
+<!-- This is the ultimate marvel index your character central for anything you wanna know about your favorite characters! -->
--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 $(() => {
 
-const userInput = $('input[type="text"]').val();
-  const marvel = $.ajax({
-    url: 'https://gateway.marvel.com:443/v1/public/characters?apikey=1e8394669669a157a9573307081972ef' + userInput,
-  });
-  marvel.then(
-    (data) => {
-      console.log(data);
-    },
-    (error) => {
-      console.log('Error');
+
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    $('form').on('reset', (event) => {
+      $('#name').text('');
+      $('#description').text('');
+      $('img').attr('src', '');
+      $('#coms').text('');
+    })
+
+    const userInput = $('input[type="text"]').val();
+    const marvel = $.ajax({
+      url: 'https://gateway.marvel.com:443/v1/public/characters?apikey=1e8394669669a157a9573307081972ef&name=' + userInput
     });
+    marvel.then(
+      (data) => {
+        console.log(data);
+        const path = data.data.results[0].thumbnail.path;
+        const exten = data.data.results[0].thumbnail.extension;
+        const url = path + '/portrait_uncanny' + "." + exten;
+        $('img').attr('src', url);
+        $('#name').html(data.data.results[0].name)
+        $('#description').html(data.data.results[0].description)
+        const unlist = $('<ul>');
+        for (let i = 0; i < 10; i++) {
+          const list = $('<li>');
+          list.html(data.data.results[0].comics.items[i].name)
+          unlist.append(list);
+        }
+        $('#coms').append(unlist);
+      },
+      (error) => {
+        console.log('Error');
+      }
+    );
+  });
 
-
-
-})
+});
